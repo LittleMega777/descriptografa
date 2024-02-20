d08defc570ac55e78d6def2134819e9997fa2160
LittleMega777 <pierrecouto02@gmail.com>
2024-02-20 20:11:29 -0300

    v1 do script principal

---
+++ descriptografa.py
@@ -0,0 +1,23 @@
+import hashlib
+
+def gerawordlist():
+    wordlist = []
+    for word in range(0, 10000):
+        if word <= 9:
+            wordlist.append(f'000{word}')
+        elif 10 <= word <= 99:
+            wordlist.append(f'00{word}')
+        elif 100 <= word <= 999:
+            wordlist.append(f'0{word}')
+        else:
+            wordlist.append(f'{word}')
+    return wordlist
+
+senha = "01846ae470651e97d2f73fce979406a9"
+
+word_list = gerawordlist()
+
+for numero in word_list:
+    tentativa = hashlib.md5(f"{numero}".encode("utf-8")).hexdigest()
+    if tentativa == senha:
+        print(f'o hash md5 {tentativa} e equivalente a {numero}')

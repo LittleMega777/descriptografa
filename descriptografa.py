1a514a9171564f50a4631684363550a547984efe
LittleMega777 <pierrecouto02@gmail.com>
2024-02-21 20:35:30 -0300

    functions upgrade

--- descriptografa.py
+++ descriptografa.py
@@ -1,6 +1,6 @@
 import hashlib # importando biblioteca hash
 
-def gerawordlist(): # gera a lista de palavras
+def gerawordlist()-> list: # gera a lista de palavras
     wordlist = []
     for word in range(0, 10000):
         if word <= 9:
@@ -13,11 +13,13 @@ def gerawordlist(): # gera a lista de palavras
             wordlist.append(f'{word}')
     return wordlist
 
-senha = "fa246d0262c3925617b0c72bb20eeb1d" # recebe a senha hash aqui em hash md5
+def descriptografa(wordlist:list, senha_cript:str)-> str:# descriptografa
+    for valor in wordlist: # para cada valor na lista
+        tentativa = hashlib.md5(f"{valor}".encode("utf-8")).hexdigest()
+    if tentativa == senha_cript:
+        print(f'o hash md5 {tentativa} e equivalente a {valor}')
 
-word_list = gerawordlist()
 
-for numero in word_list: # para cada numero na lista
-    tentativa = hashlib.md5(f"{numero}".encode("utf-8")).hexdigest()
-    if tentativa == senha:
-        print(f'o hash md5 {tentativa} e equivalente a {numero}')
+senha = "fa246d0262c3925617b0c72bb20eeb1d" # recebe a senha hash md5
+
+descriptografa(gerawordlist(), senha) # retorna o resultado
